91be7b8f2bf1d052083a3bcfc1c7689c27dc7683
Murad501 <mm845635@gmail.com>
2023-04-11 06:30:52 +0600

    project setup done

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("data is coming soon");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@user1.istzhai.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const imageCollections = client.db("dobby-ads").collection("images");
+  } catch {}
+};
+run().catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log("server running on port", port);
+});
